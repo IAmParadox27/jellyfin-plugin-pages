0ebe1786cf1a4af1c29dee4f3e6b1c5d7279b2da
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-01-19 22:18:20 +0000

    Added assembly version file for automated versioning from CI

---
+++ src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+[assembly: AssemblyCompany("IAmParadox27")]
+[assembly: AssemblyProduct("Jellyfin.Plugins.PluginPages")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Jellyfin.Plugins.PluginPages")]
+[assembly: AssemblyVersion("1.0.0.0")]
