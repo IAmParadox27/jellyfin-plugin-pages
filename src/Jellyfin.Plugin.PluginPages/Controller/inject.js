e2f142201c6f112289295f0e002b7aaf5a37a143
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-02-02 23:58:25 +0000

    Cleaned up the injected script code so that any prospective `onReady` function wouldn't interfere with ours by creating a class and using that class. Changed init logic to use mutation observer instead of waiting for click event, which stopped working for themes that didn't have a collapsable sidebar.

--- src/Jellyfin.Plugin.PluginPages/Controller/inject.js
+++ src/Jellyfin.Plugin.PluginPages/Controller/inject.js
@@ -1,52 +1,78 @@
 'use strict';
 
-$(document.body).on('click', '.headerButton[title=Menu]', function() {
-    onReady();
-    populateSidebar();
-});
+const PluginPages = {
+    initialized: false,
+    init: function () {
+        var MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
+        var myObserver          = new MutationObserver (this.mutationHandler);
+        var obsConfig           = { childList: true, characterData: true, attributes: true, subtree: true };
 
-function onReady() {
-    // When the mainDrawer first get's created, lets add the plugin pages section at the bottom
-    let length = $(".pluginMenuOptions").length;
-    
-    if (length !== 0)
-    {
-        return;
-    }
-    
-    $(".mainDrawer-scrollContainer").children('.userMenuOptions').after('<div class="pluginMenuOptions"></div>');
-}
+        $("body").each ( function () {
+            myObserver.observe (this, obsConfig);
+        } );
+    },
+    mutationHandler: function (mutationRecords) {
+        if (PluginPages.initialized) {
+            return;
+        }
+        mutationRecords.forEach ( function (mutation) {
+            console.log (mutation.type);
+            if (mutation.addedNodes && mutation.addedNodes.length > 0) {
 
-function populateSidebar() {
-    
-    if (ApiClient !== undefined && ApiClient !== null) {
-        const url = ApiClient.getUrl('PluginPages/User');
-        ApiClient.getJSON(url).then(function(items) {
+                [].some.call(mutation.addedNodes, function (addedNode) {
+                    if ($('.mainDrawer-scrollContainer').length > 0) {
+                        if ($(".mainDrawer-scrollContainer").children('.userMenuOptions').length > 0) {
+                            PluginPages.initialized = true;
+                            PluginPages.onReady();
+                            PluginPages.populateSidebar();
+                        }
+                    }
+                });
+            }
+        } );
+    },
+    onReady: function () {
+        let length = $(".pluginMenuOptions").length;
 
-            let pluginMenuOptions = $(".pluginMenuOptions")[0];
+        if (length !== 0)
+        {
+            return;
+        }
 
-            if (items.TotalRecordCount > 0) {
-                let html = `<h3 class="sidebarHeader">Plugin Settings</h3>`;
+        $(".mainDrawer-scrollContainer").children('.userMenuOptions').after('<div class="pluginMenuOptions"></div>');
+    },
+    populateSidebar: function () {
+        if (ApiClient !== undefined && ApiClient !== null) {
+            const url = ApiClient.getUrl('PluginPages/User');
+            ApiClient.getJSON(url).then(function(items) {
 
-                html += items.Items.map(function(item) {
-                    const icon = item.Icon;
-                    const itemId = item.Id;
+                let pluginMenuOptions = $(".pluginMenuOptions")[0];
 
-                    return `<a is="emby-linkbutton" data-itemid="${itemId}" class="lnkMediaFolder navMenuOption" href="#/userpluginsettings.html?pageUrl=${item.Url}">
+                if (items.TotalRecordCount > 0) {
+                    let html = `<h3 class="sidebarHeader">Plugin Settings</h3>`;
+
+                    html += items.Items.map(function(item) {
+                        const icon = item.Icon;
+                        const itemId = item.Id;
+
+                        return `<a is="emby-linkbutton" data-itemid="${itemId}" class="lnkMediaFolder navMenuOption" href="#/userpluginsettings.html?pageUrl=${item.Url}">
                                     <span class="material-icons navMenuOptionIcon ${icon}" aria-hidden="true"></span>
                                     <span class="sectionName navMenuOptionText">${item.DisplayText}</span>
                                 </a>`;
-                }).join('');
+                    }).join('');
 
-                pluginMenuOptions.innerHTML = html;
-                const elem = pluginMenuOptions;
-                const sidebarLinks = elem.querySelectorAll('.navMenuOption');
+                    pluginMenuOptions.innerHTML = html;
+                    const elem = pluginMenuOptions;
+                    const sidebarLinks = elem.querySelectorAll('.navMenuOption');
 
-                for (const sidebarLink of sidebarLinks) {
-                    sidebarLink.removeEventListener('click', onSidebarLinkClick);
-                    sidebarLink.addEventListener('click', onSidebarLinkClick);
+                    for (const sidebarLink of sidebarLinks) {
+                        sidebarLink.removeEventListener('click', onSidebarLinkClick);
+                        sidebarLink.addEventListener('click', onSidebarLinkClick);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
-}
+};
+
+PluginPages.init();
