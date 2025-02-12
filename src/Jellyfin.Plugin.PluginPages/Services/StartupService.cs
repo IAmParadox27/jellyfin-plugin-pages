39d539cffe267013abdcd491cad5efb0d5b9865e
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-02-12 13:42:26 +0000

    Updated with additional diagnostic logging and corrected a typo.

--- src/Jellyfin.Plugin.PluginPages/Services/StartupService.cs
+++ src/Jellyfin.Plugin.PluginPages/Services/StartupService.cs
@@ -30,7 +30,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "9340b171-0ae4-4d13-9970-9c4c4feba227");
                 payload.Add("fileNamePattern", "index.html");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/IndexHtml");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/IndexHtml");
 
                 payloads.Add(payload);
             }
@@ -39,7 +39,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "a837996a-2f73-4ad2-9467-e0410222d36f");
                 payload.Add("fileNamePattern", "userpluginsettings.html");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/UserPlugin/SettingsHtml");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/UserPlugin/SettingsHtml");
 
                 payloads.Add(payload);
             }
@@ -48,7 +48,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "345a80b2-2ac2-42f5-834c-809947778a64");
                 payload.Add("fileNamePattern", "user-plugin.undefined.chunk.js");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/UserPlugin/Javascript");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/UserPlugin/Javascript");
 
                 payloads.Add(payload);
             }
@@ -57,7 +57,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "47a40f72-1af9-47b9-a2eb-e073317be4b0");
                 payload.Add("fileNamePattern", "user-plugin-index-html.undefined.chunk.js");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/UserPlugin/IndexHtmlChunk");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/UserPlugin/IndexHtmlChunk");
 
                 payloads.Add(payload);
             }
@@ -66,7 +66,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "898bd8c0-70fa-4f63-a4e8-bf16de51dfe4");
                 payload.Add("fileNamePattern", "main.jellyfin.bundle.js");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/MainBundle/PluginSettingsRoute");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/MainBundle/PluginSettingsRoute");
 
                 payloads.Add(payload);
             }
@@ -75,7 +75,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "6e4bced9-d332-4b95-93f3-b82f088f56ab");
                 payload.Add("fileNamePattern", "main.jellyfin.bundle.js");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/MainBundle/RouteIds");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/MainBundle/RouteIds");
 
                 payloads.Add(payload);
             }
@@ -84,7 +84,7 @@ namespace Jellyfin.Plugin.PluginPages.Services
                 JObject payload = new JObject();
                 payload.Add("id", "a3530fb2-6d2f-4894-8ca7-691c4e98d1ed");
                 payload.Add("fileNamePattern", "runtime.bundle.js");
-                payload.Add("tranformationEndpoint", "/PluginPages/Patch/RuntimeBundle");
+                payload.Add("transformationEndpoint", "/PluginPages/Patch/RuntimeBundle");
 
                 payloads.Add(payload);
             }
