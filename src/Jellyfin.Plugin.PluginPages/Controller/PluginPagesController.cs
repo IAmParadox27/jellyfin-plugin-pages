1fb552dab6618847bf6849b790fe33928b65ac2a
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-03-01 16:54:43 +0000

    Centralised transformation patches into static class for use by other systems

--- src/Jellyfin.Plugin.PluginPages/Controller/PluginPagesController.cs
+++ src/Jellyfin.Plugin.PluginPages/Controller/PluginPagesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Text.RegularExpressions;
+using Jellyfin.Plugin.PluginPages.Helpers;
 using Jellyfin.Plugin.PluginPages.Library;
 using Jellyfin.Plugin.PluginPages.Model;
 using MediaBrowser.Model.Querying;
@@ -15,18 +16,6 @@ namespace Jellyfin.Plugin.PluginPages.Controller
         private readonly IPluginPagesManager m_pluginPagesManager;
         private readonly Assembly m_assembly;
         private readonly string m_scriptPath;
-
-        private string[] m_userPluginPagesIds = new[]
-        {
-            "'./controllers/user/plugin/index.js'",
-            "'user-plugin'"
-        };
-
-        private string[] m_userPluginPagesHtmlIds = new[]
-        {
-            "'./controllers/user/plugin/index.html'",
-            "'user-plugin-index-html'"
-        };
 
         public PluginPagesController(IPluginPagesManager pluginPagesManager)
         {
@@ -87,82 +76,43 @@ namespace Jellyfin.Plugin.PluginPages.Controller
         [HttpPost("Patch/IndexHtml")]
         public ActionResult PatchIndexHtml([FromBody] PatchRequestPayload payload)
         {
-            string scriptElement = "<script plugin=\"PluginPages\" version=\"1.0.0.0\" src=\"/PluginPages/inject.js\" defer></script>";
-
-            string regex = Regex.Replace(payload.Contents!, "(</body>)", $"{scriptElement}$1");
-
-            return Content(regex, "text/html");
+            return Content(TransformationPatches.IndexHtml(payload), "text/html");
         }
         
         [HttpPost("Patch/UserPlugin/SettingsHtml")]
         public ActionResult PatchUserPluginSettingsHtml([FromBody] PatchRequestPayload payload)
         {
-            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
-            using StreamReader textReader = new StreamReader(fileStream);
-
-            return Content(textReader.ReadToEnd(), "text/html");
+            return Content(TransformationPatches.SettingsHtml(payload), "text/html");
         }
         
         [HttpPost("Patch/UserPlugin/Javascript")]
         public ActionResult PatchUserPluginJavascript([FromBody] PatchRequestPayload payload)
         {
-            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
-
-            using StreamReader textReader = new StreamReader(fileStream);
-            using StringWriter textWriter = new StringWriter();
-            textWriter.WriteLine("\"use strict\";");
-            textWriter.Write($"(self.webpackChunk = self.webpackChunk || []).push([[{m_userPluginPagesIds[1]}], {{{m_userPluginPagesIds[0]}:function(a,e,t){{t.r(e),e.default = '");
-            textWriter.Write(textReader.ReadToEnd().Replace("\r", "").Replace("\n", "").Replace("'", "\\'"));
-            textWriter.Write("'}}]);");
-            
-            return Content(textWriter.ToString(), "application/javascript");
+            return Content(TransformationPatches.UserPluginJs(payload), "application/javascript");
         }
         
         [HttpPost("Patch/UserPlugin/IndexHtmlChunk")]
         public ActionResult PatchUserPluginIndexHtmlJavascript([FromBody] PatchRequestPayload payload)
         {
-            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
-
-            using StreamReader textReader = new StreamReader(fileStream);
-            using StringWriter textWriter = new StringWriter();
-            textWriter.WriteLine("\"use strict\";");
-            textWriter.Write($"(self.webpackChunk = self.webpackChunk || []).push([[{m_userPluginPagesHtmlIds[1]}], {{{m_userPluginPagesHtmlIds[0]}:function(a,e,t){{t.r(e),e.default = '");
-            textWriter.Write(textReader.ReadToEnd().Replace("\r", "").Replace("\n", "").Replace("'", "\\'"));
-            textWriter.Write("'}}]);");
-            
-            return Content(textWriter.ToString(), "application/javascript");
+            return Content(TransformationPatches.UserPluginIndexHtml(payload), "application/javascript");
         }
         
         [HttpPost("Patch/MainBundle/PluginSettingsRoute")]
         public ActionResult PatchMainBundle_PluginSettingsRoute([FromBody] PatchRequestPayload payload)
         {
-            string scriptElement = @"path:""userpluginsettings.html"",pageProps:{controller:""user/plugin/index"",view:""user/plugin/index.html""}},{";
-
-            string regex = Regex.Replace(payload.Contents!, "(path:\"queue\")", $"{scriptElement}$1");
-            
-            return Content(regex, "application/javascript");
+            return Content(TransformationPatches.MainBundlePluginSettingsRoute(payload), "application/javascript");
         }
         
         [HttpPost("Patch/MainBundle/RouteIds")]
         public ActionResult PatchMainBundle_RouteIds([FromBody] PatchRequestPayload payload)
         {
-            string scriptElement = @$"""./user/plugin/index"":[{string.Join(',', m_userPluginPagesIds)}],";
-            scriptElement += @$"""./user/plugin/index.html"":[{string.Join(',', m_userPluginPagesHtmlIds)}],";
-            
-            string regex = Regex.Replace(payload.Contents!, "(\"\\.\\/home\\.html\")", $"{scriptElement}$1");
-            
-            return Content(regex, "application/javascript");
+            return Content(TransformationPatches.MainBundleRouteIds(payload), "application/javascript");
         }
         
         [HttpPost("Patch/RuntimeBundle")]
         public ActionResult PatchRuntimeBundle([FromBody] PatchRequestPayload payload)
         {
-            string scriptElement = @$"{m_userPluginPagesIds[1]}:""user-plugin"",";
-            scriptElement += @$"{m_userPluginPagesHtmlIds[1]}:""user-plugin-index-html"",";
-            
-            string regex = Regex.Replace(payload.Contents!, "(8372:\"home-html\")", $"{scriptElement}$1");
-            
-            return Content(regex, "application/javascript");
+            return Content(TransformationPatches.RuntimeBundle(payload), "application/javascript");
         }
     }
 }
---
+++ src/Jellyfin.Plugin.PluginPages/Helpers/TransformationPatches.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using Jellyfin.Plugin.PluginPages.Model;
+
+namespace Jellyfin.Plugin.PluginPages.Helpers
+{
+    public static class TransformationPatches
+    {
+        private static string[] s_userPluginPagesIds = new[]
+        {
+            "'./controllers/user/plugin/index.js'",
+            "'user-plugin'"
+        };
+
+        private static string[] s_userPluginPagesHtmlIds = new[]
+        {
+            "'./controllers/user/plugin/index.html'",
+            "'user-plugin-index-html'"
+        };
+        
+        public static string IndexHtml(PatchRequestPayload payload)
+        {
+            string scriptElement = "<script plugin=\"PluginPages\" version=\"1.0.0.0\" src=\"/PluginPages/inject.js\" defer></script>";
+
+            string regex = Regex.Replace(payload.Contents!, "(</body>)", $"{scriptElement}$1");
+
+            return regex;
+        }
+
+        public static string SettingsHtml(PatchRequestPayload payload)
+        {
+            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
+            using StreamReader textReader = new StreamReader(fileStream);
+            
+            return textReader.ReadToEnd();
+        }
+
+        public static string UserPluginJs(PatchRequestPayload payload)
+        {
+            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
+
+            using StreamReader textReader = new StreamReader(fileStream);
+            using StringWriter textWriter = new StringWriter();
+            textWriter.WriteLine("\"use strict\";");
+            textWriter.Write($"(self.webpackChunk = self.webpackChunk || []).push([[{s_userPluginPagesIds[1]}], {{{s_userPluginPagesIds[0]}:function(a,e,t){{t.r(e),e.default = '");
+            textWriter.Write(textReader.ReadToEnd().Replace("\r", "").Replace("\n", "").Replace("'", "\\'"));
+            textWriter.Write("'}}]);");
+            
+            return textWriter.ToString();
+        }
+
+        public static string UserPluginIndexHtml(PatchRequestPayload payload)
+        {
+            Stream fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(PluginPagesPlugin).Namespace}.Controller.userpluginsettings.html")!;
+
+            using StreamReader textReader = new StreamReader(fileStream);
+            using StringWriter textWriter = new StringWriter();
+            textWriter.WriteLine("\"use strict\";");
+            textWriter.Write($"(self.webpackChunk = self.webpackChunk || []).push([[{s_userPluginPagesHtmlIds[1]}], {{{s_userPluginPagesHtmlIds[0]}:function(a,e,t){{t.r(e),e.default = '");
+            textWriter.Write(textReader.ReadToEnd().Replace("\r", "").Replace("\n", "").Replace("'", "\\'"));
+            textWriter.Write("'}}]);");
+            
+            return textWriter.ToString();
+        }
+
+        public static string MainBundlePluginSettingsRoute(PatchRequestPayload payload)
+        {
+            string scriptElement = @"path:""userpluginsettings.html"",pageProps:{controller:""user/plugin/index"",view:""user/plugin/index.html""}},{";
+
+            string regex = Regex.Replace(payload.Contents!, "(path:\"queue\")", $"{scriptElement}$1");
+            
+            return regex;
+        }
+
+        public static string MainBundleRouteIds(PatchRequestPayload payload)
+        {
+            string scriptElement = @$"""./user/plugin/index"":[{string.Join(',', s_userPluginPagesIds)}],";
+            scriptElement += @$"""./user/plugin/index.html"":[{string.Join(',', s_userPluginPagesHtmlIds)}],";
+            
+            string regex = Regex.Replace(payload.Contents!, "(\"\\.\\/home\\.html\")", $"{scriptElement}$1");
+            
+            return regex;
+        }
+
+        public static string RuntimeBundle(PatchRequestPayload payload)
+        {
+            string scriptElement = @$"{s_userPluginPagesIds[1]}:""user-plugin"",";
+            scriptElement += @$"{s_userPluginPagesHtmlIds[1]}:""user-plugin-index-html"",";
+            
+            string regex = Regex.Replace(payload.Contents!, "(8372:\"home-html\")", $"{scriptElement}$1");
+            
+            return regex;
+        }
+    }
+}
