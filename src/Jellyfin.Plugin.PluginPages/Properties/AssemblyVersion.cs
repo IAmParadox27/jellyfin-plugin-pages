164ec537d6c42cae577afdd082773eb225f0c9e5
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-04-18 21:02:45 +0100

    Updated to support 10.10.7

--- src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
+++ src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyProduct("Jellyfin.Plugins.PluginPages")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugins.PluginPages")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
