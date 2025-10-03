fae0033e1ddf8f5501900734fc42fa6aeefeeb8b
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-10-03 21:18:22 +0100

    Changed 10.11 pre-release code to target rc8 not rc5

--- src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
+++ src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
@@ -6,4 +6,4 @@ using Jellyfin.Plugin.PluginPages.Attributes;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugins.PluginPages")]
 [assembly: AssemblyVersion("2.3.0.0")]
-[assembly: JellyfinVersion("10.11.0-rc5")]
+[assembly: JellyfinVersion("10.11.0-rc8")]
