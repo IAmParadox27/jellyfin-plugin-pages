171a51d9dc9853441c5c91aef309e5df564a9e7f
josh <josh@JOSH1>
2025-12-22 23:16:16 +0000

    update version

--- src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
+++ src/Jellyfin.Plugin.PluginPages/Properties/AssemblyVersion.cs
@@ -5,5 +5,5 @@ using Jellyfin.Plugin.PluginPages.Attributes;
 [assembly: AssemblyProduct("Jellyfin.Plugins.PluginPages")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("Jellyfin.Plugins.PluginPages")]
-[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("1.0.3")]
 [assembly: JellyfinVersion("10.11.2")]
