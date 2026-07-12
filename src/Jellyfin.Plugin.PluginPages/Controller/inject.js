c850937df1e804275aac6abd9f054d449e9d82a4
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2026-07-12 17:01:14 +0100

    Fixed injection for plugin pages to work with v12

--- src/Jellyfin.Plugin.PluginPages/Controller/inject.js
+++ src/Jellyfin.Plugin.PluginPages/Controller/inject.js
@@ -39,7 +39,13 @@ const PluginPages = {
             return;
         }
 
-        $(".mainDrawer-scrollContainer").children('.userMenuOptions').after('<div class="pluginMenuOptions"></div>');
+        var newLayoutSelector = $("[href='#/mypreferencesmenu']");
+        
+        if (newLayoutSelector.length === 0) {
+            $(".mainDrawer-scrollContainer").children('.userMenuOptions').after('<div class="pluginMenuOptions"></div>');
+        } else {
+            newLayoutSelector.after('<div class="pluginMenuOptions marker"></div>');
+        }
     },
     populateSidebar: function () {
         if (ApiClient !== undefined && ApiClient !== null) {
@@ -51,17 +57,45 @@ const PluginPages = {
                 if (items.TotalRecordCount > 0) {
                     let html = `<h3 class="sidebarHeader">Plugin Settings</h3>`;
 
-                    html += items.Items.map(function(item) {
-                        const icon = item.Icon;
-                        const itemId = item.Id;
+                    function getNextHtml(items) {
+                        return items.filter(function (value, index, array) {
+                            return array.map(function (i) { return i.Id; }).indexOf(value.Id) === index;
+                        }).map(function(item) {
+                            const icon = item.Icon;
+                            const itemId = item.Id;
 
-                        return `<a is="emby-linkbutton" data-itemid="${itemId}" class="lnkMediaFolder navMenuOption" href="#/userpluginsettings.html?pageUrl=${item.Url}">
-                                    <span class="material-icons navMenuOptionIcon ${icon}" aria-hidden="true"></span>
-                                    <span class="sectionName navMenuOptionText">${item.DisplayText}</span>
-                                </a>`;
-                    }).join('');
+                            const container = $(".pluginMenuOptions");
+                            const type = container.hasClass("marker") ? "paper" : "classic";
+                            if (type === 'paper') {
+                                return `<a class="MuiButtonBase-root MuiMenuItem-root MuiMenuItem-gutters MuiMenuItem-root MuiMenuItem-gutters css-mbeig7" tabindex="-1" role="menuitem" href="#/userpluginsettings.html?pageUrl=${item.Url}" data-plugin-pages="true" >
+                                        <div class="MuiListItemIcon-root css-5pks8q">
+                                            <span class="MuiButton-icon MuiButton-startIcon MuiButton-iconSizeMedium css-1ygddt1 material-icons ${icon}">
+                                            </span>
+                                        </div>
+                                        <div class="MuiListItemText-root css-t3p1a1">
+                                            <span class="MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-pl8nxc">${item.DisplayText}</span>
+                                        </div>
+                                    </a>`;
+                            } else {
+                                return `<a is="emby-linkbutton" data-itemid="${itemId}" class="lnkMediaFolder navMenuOption" href="#/userpluginsettings.html?pageUrl=${item.Url}" data-plugin-pages="true">
+                                        <span class="material-icons navMenuOptionIcon ${icon}" aria-hidden="true"></span>
+                                        <span class="sectionName navMenuOptionText">${item.DisplayText}</span>
+                                    </a>`;
+                            }
+                        }).join('');
+                    }
+                    
+                    let nextHtml = getNextHtml(items.Items);
 
-                    pluginMenuOptions.innerHTML = html;
+                    // Remove all previous plugin pages
+                    $("[data-plugin-pages=true]").remove();
+                    
+                    const container = $(".pluginMenuOptions");
+                    if (container.hasClass("marker")) {
+                        container.after(`<hr data-plugin-pages="true" class="MuiDivider-root MuiDivider-fullWidth css-14093q0" />${nextHtml}`);
+                    } else {
+                        container[0].innerHTML = nextHtml;
+                    }
                 }
             });
         }
